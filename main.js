4fdf0d423c9a7741f79ace97372277e117cf2ca9
RajatSaini90 <sainirohan695@gmail.com>
2024-03-16 01:40:24 +0530

    pop changes

--- main.js
+++ main.js
@@ -1,155 +1,152 @@
+let popContainers = document.querySelectorAll(
+  ".popular-container .custom-card"
+);
+
+popContainers.forEach((card) => {
+  card.addEventListener("click", function () {
+    if (card.style.height === "500px") {
+      card.style.height = "";
+    } else {
+      card.style.height = "500px";
+    }
+  });
+});
+
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-function scrollHeader(){
-
-    const header = document.getElementById('header');
-    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 50) header.classList.add('scroll-header');
-    else header.classList.remove('scroll-header');
+function scrollHeader() {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
 }
-window.addEventListener('scroll', scrollHeader);
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SWIPER POPULAR ===============*/
 
-var swiperPopular = new Swiper(".popular__container",{
+var swiperPopular = new Swiper(".popular__container", {
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
 
-    spaceBetween: 32,
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView:'auto',
-    loop: true,
-
-    navigation:{
-
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
-
 
 /*=============== VALUE ACCORDION ===============*/
 
-const accordionItems = document.querySelectorAll('.value__accordion-item');
+const accordionItems = document.querySelectorAll(".value__accordion-item");
 
-accordionItems.forEach((item) =>{
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector(".value__accordion-header");
 
-    const accordionHeader = item.querySelector('.value__accordion-header');
+  accordionHeader.addEventListener("click", () => {
+    const openItem = document.querySelector(".accordion-open");
 
-    accordionHeader.addEventListener('click', () => {
+    toggleItem(item);
 
-        const openItem = document.querySelector('.accordion-open')
-
-        toggleItem(item)
-
-        if(openItem && openItem !== item){
-
-            toggleItem(openItem)
-        }
-
-    });
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
+    }
+  });
 });
 
 const toggleItem = (item) => {
+  const accordionContent = item.querySelector(".value__accordion-content");
 
-    const accordionContent =   item.querySelector('.value__accordion-content');
-
-    if(item.classList.contains('accordion-open')){
-
-        accordionContent.removeAttribute('style')
-        item.classList.remove('accordion-open')
-
-    }else{
-
-        accordionContent.style.height = accordionContent.scrollHeight + 'px'
-        item.classList.add('accordion-open')
-
-    }
-
+  if (item.classList.contains("accordion-open")) {
+    accordionContent.removeAttribute("style");
+    item.classList.remove("accordion-open");
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + "px";
+    item.classList.add("accordion-open");
+  }
 };
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+const sections = document.querySelectorAll("section[id]");
 
-const sections = document.querySelectorAll('section[id]')
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-function scrollActive(){
-    
-        const scrollY = window.pageYOffset
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
 
-        sections.forEach(current =>{
-
-            const sectionHeight = current.offsetHeight,
-                        sectionTop = current.offsetTop - 58,
-                        sectionId = current.getAttribute('id')
-
-            if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-
-                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-
-            }else{
-
-                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-            }
-        })
-
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
 }
-window.addEventListener('scroll', scrollActive)
+window.addEventListener("scroll", scrollActive);
 
+/*=============== SHOW SCROLL UP ===============*/
 
-/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-
 
-function scrollUp(){
-
-    const scrollUp = document.getElementById('scroll-up');
-    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-
-
-    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
-
+  if (this.scrollY >= 350) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
 }
-window.addEventListener('scroll', scrollUp)
+window.addEventListener("scroll", scrollUp);
 
+/*=============== DARK LIGHT THEME ===============*/
 
-/*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark__theme";
+const iconTheme = "bx-sun";
 
-const themeButton = document.getElementById('theme-button');
-const darkTheme = 'dark__theme';
-const iconTheme = 'bx-sun';
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
-const selectedTheme = localStorage.getItem('selected-theme');
-const selectedIcon = localStorage.getItem('selected-icon');
-
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun';
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "bx bx-moon" : "bx bx-sun";
 
 if (selectedTheme) {
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
-  themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme);
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "bx bx-moon" ? "add" : "remove"](
+    iconTheme
+  );
 }
 
-themeButton.addEventListener('click', () => {
+themeButton.addEventListener("click", () => {
   document.body.classList.toggle(darkTheme);
   themeButton.classList.toggle(iconTheme);
 
-  localStorage.setItem('selected-theme', getCurrentTheme());
-  localStorage.setItem('selected-icon', getCurrentIcon());
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
 });
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
+/*=============== LOGIN FORM ===============*/
 
+let loginForm = document.querySelector(".my-form");
 
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let email = document.getElementById("email");
+  let passward = document.getElementById("passward");
 
-  /*=============== LOGIN FORM ===============*/
+  console.log("Emai : ", email.value);
+  console.log("Passward : ", passward.value);
+});
 
-  let loginForm = document.querySelector(".my-form");
-
-  loginForm.addEventListener("submit", (e) => {
-
-    e.preventDefault();
-    let email = document.getElementById("email");
-    let passward = document.getElementById("passward");
-
-    console.log('Emai : ', email.value);
-    console.log('Passward : ', passward.value);
-    
-  })
+/*==============================*/
